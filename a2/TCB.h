9940f5d4a19041c3218a5ac1f4c677098ef6ed0a
radleigh <radleigh@msn.com>
2017-06-30 01:21:22 -0700

    added further revisions to TCB.h

--- a2/TCB.h
+++ a2/TCB.h
@@ -1,11 +1,25 @@
+// Declares the prototypes for the tasks
+
+#ifndef HEAD_TCB
+#define HEAD_TCB
+
+// function prototypes
+// --- Insert here ---
+
+
 // Declares a TCB structure
 struct myStruct
 {
+    // Pointer to a function taking void* arg and returning a void	
     void (*myTask)(void*);
+
+    // Pointer to void used to reference the shared data for the task
     void *taskDataPtr;
 };
 typedef struct myStruct TCB;
 
-// Emulates the Boolean type
+// Enumerates the Boolean type
 enum myBool { FALSE = 0, TRUE = 1 };
 typedef enum _myBool Bool;
+
+#endif
