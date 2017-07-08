f6956da32bb78fdb000ae91d7fa4cf61e580b05a
Daniel <snitkdan@uw.edu>
2017-07-08 09:36:54 -0700

    Fix file path

--- a2/powerSubsystemTest.cc
+++ a2/powerSubsystemTest.cc
@@ -6,7 +6,7 @@
 */
 
 #include "../gtest/gtest.h"
-#include "../powerSubsystem.h"
+#include "powerSubsystem.h"
 
 // This tests the "powerConsumption" method in "powerSubsystem"
 TEST(PowerSubsystemTest, Test_PowerConsumption) {
