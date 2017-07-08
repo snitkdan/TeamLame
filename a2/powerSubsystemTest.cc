c12428e3ccb44c2e2d3313026c1f6a795cbdaaea
Daniel <snitkdan@uw.edu>
2017-07-08 10:04:50 -0700

    externing "C"

--- a2/powerSubsystemTest.cc
+++ a2/powerSubsystemTest.cc
@@ -6,7 +6,10 @@
 */
 
 #include "../gtest/gtest.h"
-#include "powerSubsystem.h"
+extern "C" {
+  #include "powerSubsystem.h"  
+}
+
 
 // This tests the "powerConsumption" method in "powerSubsystem"
 TEST(PowerSubsystemTest, Test_PowerConsumption) {
