e9a4898bd97164cce6ec6c4a0863109e4d90de5b
Daniel <snitkdan@uw.edu>
2017-07-09 20:28:19 -0700

    Comments

--- a2/thrusterSubsystemTest.cc
+++ a2/thrusterSubsystemTest.cc
@@ -11,40 +11,41 @@ extern "C" {
     #include <stdint.h>
 }
 
-// 0. Declare test variables
-uint8_t left = 0x00;
-uint8_t up = 0x02;
-uint8_t down = 0x03;
-uint8_t thrust_dir[3] = {left, up, down};
-
-uint8_t off = 0x00;
-uint8_t twenty_prct = 0x03;
-uint8_t max = 0x0F;
-uint8_t magnitude[3] = {off, twenty_prct, max};
-
-uint8_t zero_sec = 0x00;
-uint8_t oneeighty_sec  = 0xB4;
-uint8_t twofiftyfive_sec = 0xFF;
-uint8_t duration[3] = {zero_sec, oneeighty_sec, twofiftyfive_sec};
-// 1. Construct thrust commands from
-// all combinations of test variables
-unsigned int thrusterCommands[3][3][3];
-int i, j, k;
-for(i = 0; i < 3; i++) {
-  uint8_t dir = thrust_dir[i];
-  for(j = 0; j < 3; j++) {
-    uint8_t mag = magnitude[j];
-    for(k = 0; k < 3; k++) {
-      uint8_t dur = duration[k];
-      unsigned int thrusterCommand = (dur << 8)|(mag << 4)|(dir);
-      thrusterCommands[i][j][k] = thrusterCommand;
-    }
-  }
-}
-
 // This tests the "parseCommands" method in "thrusterSubsystem.c"
 TEST(ThrusterSubsystemTest, Test_ParseCommands) {
-  // 1. Cycle through all thrusterCommands and
+  // 0. Declare test variables
+  uint8_t left = 0x00;
+  uint8_t up = 0x02;
+  uint8_t down = 0x03;
+  uint8_t thrust_dir[3] = {left, up, down};
+
+  uint8_t off = 0x00;
+  uint8_t twenty_prct = 0x03;
+  uint8_t max = 0x0F;
+  uint8_t magnitude[3] = {off, twenty_prct, max};
+
+  uint8_t zero_sec = 0x00;
+  uint8_t oneeighty_sec  = 0xB4;
+  uint8_t twofiftyfive_sec = 0xFF;
+  uint8_t duration[3] = {zero_sec, oneeighty_sec, twofiftyfive_sec};
+
+  // 1. Construct thrust commands from
+  // all combinations of test variables
+  unsigned int thrusterCommands[3][3][3];
+  int i, j, k;
+  for(i = 0; i < 3; i++) {
+    uint8_t dir = thrust_dir[i];
+    for(j = 0; j < 3; j++) {
+      uint8_t mag = magnitude[j];
+      for(k = 0; k < 3; k++) {
+        uint8_t dur = duration[k];
+        unsigned int thrusterCommand = (dur << 8)|(mag << 4)|(dir);
+        thrusterCommands[i][j][k] = thrusterCommand;
+      }
+    }
+  }
+
+  // 2. Cycle through all thrusterCommands and
   // compare to returned value.
   cleanCommands cc;
   for(i = 0; i < 3; i++) {
@@ -69,14 +70,14 @@ TEST(ThrusterSubsystemTest, Test_GetCost) {
   // compare to returned value.
   cleanCommands cc;
   for(i = 0; i < 3; i++) {
-    uint8_t dir = thrust_dir[i];
+    //uint8_t dir = thrust_dir[i];
     for(j = 0; j < 3; j++) {
-      uint8_t mag = magnitude[j];
+      //uint8_t mag = magnitude[j];
       for(k = 0; k < 3; k++) {
-        uint8_t dur = duration[k];
-        unsigned int thrusterCommand = thrusterCommands[i][j][k];
+        //uint8_t dur = duration[k];
+        /*unsigned int thrusterCommand = thrusterCommands[i][j][k];
         parseCommands(&thrusterCommand, &cc);
-        unsigned short fuelCost = getFuelCost(&cc);
+        unsigned short fuelCost = getFuelCost(&cc);*/
         unsigned short expectedFuelCost = 1;
         ASSERT_EQ(expectedFuelCost, 1);
       }
