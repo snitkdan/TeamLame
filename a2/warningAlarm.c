4e26df0feac3358cf5a5117ed67989b3a5690584
Daniel <snitkdan@uw.edu>
2017-07-10 15:35:25 -0700

    Initial draft of warningAlarm

--- a2/warningAlarm.c
+++ a2/warningAlarm.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdio.h>
 #include "dataStructs.h"
 
+// Structure for holding LED metadata
+typedef struct LED_STRUCT {
+  char *path;  // system file path to LED
+  FILE *file;  // file pointer (NULL if not open)
+  unsigned int sec;  // blink duration (-1 if continuous)
+  bool active;  // true of on, false otherwise
+} LED;
 
 void warningAlarm(void *warnStruct) {
   // 0. Store warning data in local variables
@@ -11,38 +19,62 @@ void warningAlarm(void *warnStruct) {
   unsigned short *batteryLvlPtr = wData->batteryLvlPtr;
   unsigned short *fuelLvlPtr = wData->fuelLvlPtr;
 
-  // 1. Interrogate batteryLvl && fuelLvl
-  switch(*batteryLvl) {
-      case
+  // 1. Declare file paths for LEDs
+  static char *l1 = "sys/class/leds/beaglebone:green:usr1/brightness";
+  static char *l2 = "sys/class/leds/beaglebone:green:usr2/brightness";
+  static char *l3 = "sys/class/leds/beaglebone:green:usr3/brightness";
+
+  // 2. Declare structures to store LED metadata
+  static LED led1 = {l1, NULL, 0, FALSE};
+  static LED led2 = {l2, NULL, 0, FALSE};
+  static LED led3 = {l3, NULL, 0, FALSE};
+  static LED *leds[3] = {&led1, &led2, &led3};
+
+  // 3. Interrogate batteryLvl && fuelLvl
+  if(*batteryLvlPtr > 50 && *fuelLvlPtr > 50) {
+    // 3.1: No alert.
+    //led3.file = fopen(led3.path);
+    led3.sec = -1; // "infinity"
+    led3.active = true;
+    *fuelLowPtr = false;
+    *batteryLowPtr = false;
+  } else {
+    // 3.2: Alert
+    if(*batteryLvl <= 50) {
+      // 3.2.1: Battery level low
+      //led2.file = fopen(led2.path);
+      led2.active = true;
+      led2.sec = (*batteryLvl <= 10) ? 1 : 2;
+      *batteryLowPtr = (*batteryLvl <= 10) ? true : false;
+    }
+    if(*fuelLvl <= 50) {
+      // 3.2.2: Fuel level low
+      //led1.file = fopen(led1.path);
+      led1.active = true;
+      led1.sec = (*fuelLvlPtr <= 10) ? 1 : 2;
+      *fuelLowPtr = (*fuelLvlPtr <= 10) ? true : false;
+    }
   }
 
-/*
-     if (batt Lvl > 50 && fuelLvl > 50) {
-         turn on LED 3
-         fuelLow = false;
-         battLow = false;
+  // 4. Display/Deactive appropriate LEDs.
+  int i = 0;
+  while(i < 3) {
+    if(leds[i]->active) {
+      display(leds[i]);
     } else {
-         turn off LED3
-        // batt
-        if (10 < battLvl <= 50) {
-            blink LED2 at 2 sec
+      deactivate(leds[i]);
+    }
+  }
+}
 
-        } else if ( battLvl <= 10) {
-            blink LED2 at 1 sec
-            battLow = true;
-        } else {// batt lvl is good, but we know fuel lvl is low
-            do nothing
-        }
+void display(LED *led) {
 
-        // fuel
-        if (10 < fuelLvl <= 50) {
-            blink LED1 at 2 sec
-        } else if ( fuelLvl <= 10) {
-            blink LED1 at 1 sec
-            fuelLow = true;
-        } else {// fuel lvl is good, but we know batt lvl waslow
-            do nothing
-        }
-    }*/
+}
 
+void deactivate(LED *led) {
+  // 0. Close the connection (turn off LED)
+  fclose(led->file);
+  // 1. Reassign struct fields
+  led->file = NULL;
+  led->active = false;
 }
---
+++ a2/warningAlarm.h
@@ -0,0 +1,62 @@
+#include <stdio.h>
+
+// Structure for holding LED metadata
+typedef struct LED_STRUCT {
+  char *path;  // system file path to LED
+  FILE *file;  // file pointer (NULL if not open)
+  unsigned int sec;  // blink duration (-1 if continuous)
+  bool active;  // true of on, false otherwise
+} LED;
+
+/*
+  @param warnStruct
+    Struct containing necessary
+    data for the warningAlarm
+  @modifies
+    *warnStruct.fuelLowPtr
+    && *warnStruct.batteryLowPtr
+    && beaglebone LEDs 1-3
+  @effects
+    updates fuelLowPtr && batteryLowPtr
+    based on fuelLvlPtr && batteryLvlPtr,
+    respectively. There are several cases
+    involving these variables that affect
+    the beaglebone LEDs:
+
+    "Alert Cases":
+      1. 10 < fuelLvl <= 50: blink LED1 every 2 sec.
+      2. fuelLvl <= 10: blink LED1 every 1 sec.
+      3. 10 < batteryLvl <= 50: blink LED2 every 2 sec.
+      4. fuelLvl <= 10: blink LED1 every 1 sec.
+
+    "Safe Cases":
+      1. batteryLvl && fuelLvl > 50: Illuminate LED3 continuously
+
+*/
+void warningAlarm(void *warnStruct);
+
+/*
+  @param led
+    struct containing metadata about the
+    beaglebone LED to be displayed.
+  @modifies
+    beaglebone LED specified in led
+  @effects
+    blinks beaglebone LED specified in
+    led every led.sec seconds.
+*/
+void display(LED *led);
+
+/*
+  @param led
+    struct containing metadata about the
+    beaglebone LED to be displayed.
+  @modifies
+    beaglebone LED specified in led
+  @effects
+    turns off the beaglebone LED
+    specified in led, and updates
+    the relevant fields in led
+    to indicate deactivation. 
+*/
+void deactivate(LED *led);
