47d7096930bda81e5e473a55ebd5e06673b621e7
radleigh <radleigh@uw.edu>
2017-07-09 19:31:57 -0700

    added random number generator that thrusterCommand gets in satCom.c

---
+++ a2/rand2.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <math.h>
+#include <float.h>
+#include <limits.h>
+
+//  random number generator 
+int reseed(void);
+double randomReal(void);
+
+int seed = 1;
+int multiplier = 2743;
+int addOn = 5923;
+double max = INT_MAX + 1.0;
+
+//  demonstrate the generators
+int randomInteger(int low, int high)
+{
+    if (low > high)
+        return randomInteger(high, low);
+    else
+        return ((int)((high-low+1)*randomReal()))+low;
+}
+
+int reseed(void)
+{
+    seed = seed*multiplier + addOn;
+    return seed;
+}
+
+//  generate a real random number
+double randomReal(void)
+{
+    double temp = reseed();
+    return temp/max;
+}
+
+
+
--- a2/satelliteComs.c
+++ a2/satelliteComs.c
@@ -1,13 +1,27 @@
-#include <stdio.h>
-#include "dataStructs.h"
-
-// Refer to p.13
-void satelliteComs(void *satStruct) {
 /*
-    satData *pData = (satData*)satStruct;
-    unsigned short *pGeneratePtr = pData->pGeneratePtr;
-    unsigned short pGenerateData = *pGeneratePtr;
-    printf("From satCom: %hu\n", pGenerateData);
-    ++(*pGeneratePtr);
+ * satelliteComs.c - This file contains
+ * the implementation of satelliteComs
+ * (see satelliteComs.h for specification)
 */
+
+#include <stdio.h>
+#include <stdbool.h>
+#include "dataStructs.h"
+#include "TCB.h"
+
+void satelliteComs(void *sData) {
+    // 1. Assign the data of satStruct into local variables
+    satData *satStruct = (satData*)sData;
+    bool *fuelLow = satStruct->fuelLowPtr;
+    bool *batteryLow = satStruct->batteryLowPtr;
+    bool *solarPanelState = satStruct->solarPanelStatePtr;
+    unsigned short *batteryLvl = satStruct->batteryLvlPtr;
+    unsigned short *fuelLvl = satStruct->fuelLvlPtr;
+    unsigned short *pConsume = satStruct->pConsumePtr;
+    unsigned short *pGenerate = satStruct->pGeneratePtr;
+    unsigned int *thrusterCommand = satStruct->thrusterCommandPtr;
+    
+    // 2. Retrieve random number and assign thrusterCommand to it
+    *thrusterCommand = randomInteger(0,10)%7;
+    printf("thrusterCommand = %d\n", *thrusterCommand);
 }
