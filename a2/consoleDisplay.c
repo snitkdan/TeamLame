a1bb119780cb1a3699d4f49fcbd53b6f0e89cab4
radleigh <radleigh@uw.edu>
2017-07-10 15:58:25 -0700

    Added comments to consoleDisplay, updated MakefileRad to include warningAlarm.h

--- a2/consoleDisplay.c
+++ a2/consoleDisplay.c
@@ -8,6 +8,17 @@
 #include "dataStructs.h"
 #include "TCB.h"
 
+/*
+  @param consoleStruct
+    struct containing the shared 
+    data for consoleDisplay.
+  @modifies
+    nothing    
+  @effects
+    stores the data from the shared variables
+    in the form of satelliteStatus and 
+    annunciation, passing them to terminalComs.c
+*/
 void consoleDisplay(void *consoleStruct) {
     // 1.1 Assign the data of consoleStruct into local variables
     consoleData *cData = (consoleData*)consoleStruct;
@@ -17,7 +28,7 @@ void consoleDisplay(void *consoleStruct) {
     unsigned short *batteryLvl = cData->batteryLvlPtr;
     unsigned short *fuelLvl = cData->fuelLvlPtr;
     unsigned short *pConsume = cData->pConsumePtr;
-    unsigned short *pGenerate = cData->pGeneratePtr;
+    // unsigned short *pGenerate = cData->pGeneratePtr; // FOR FUTURE PROJECTS
 
     // DEBUG. Temporary flipping of 1 and 0 (won't be part of the final code)
     static int numCalls = 0;
@@ -25,10 +36,10 @@ void consoleDisplay(void *consoleStruct) {
 
     // 1.2 Define necessary string storage 
     char *solarPanelString = (*solarPanelState) ? "Deployed":"Retracted";
-    //char *fuelString = (*fuelLow)? "YES":"NO";
-    //char *battString = (*batteryLow)? "YES":"NO";
-    char *fuelString = (numCalls == 0)? "YES":"NO";
-    char *battString = (numCalls == 1)? "YES":"NO";
+    char *fuelString = (*fuelLow)? "YES":"NO";
+    char *battString = (*batteryLow)? "YES":"NO";
+    //char *fuelString = (numCalls == 0)? "YES":"NO";
+    //char *battString = (numCalls == 1)? "YES":"NO";
     char satelliteStatus[100];
     char annunciation[100];
     
@@ -49,6 +60,6 @@ void consoleDisplay(void *consoleStruct) {
     
     // 4. Pass in the strings to terminalComs, which will
     //    be displayed on this terminal and another terminal. 
-    //terminalComs(satelliteStatus, annunciation);
-    terminalComs(annunciation, satelliteStatus);
+    terminalComs(satelliteStatus, annunciation);
+    //terminalComs(annunciation, satelliteStatus);
 }
