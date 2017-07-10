b2dae8e8e5377ca592430562540c6aa1e6b36bd9
Daniel <snitkdan@uw.edu>
2017-07-09 22:10:01 -0700

    Refactor

--- a2/thrusterSubsystem.c
+++ a2/thrusterSubsystem.c
@@ -17,7 +17,8 @@ void thrusterSubsystem(void *thrustStruct) {
   parseCommands(thrusterCommand, &cc);
 
   // 3. Calculate the fuel cost
-  static double fuelCost = ((static double)*fuelLvl) - getFuelCost(&cc);
+  static double fuelCost = 100;
+  fuelCost -= getFuelCost(&cc);
 
   // 4. Update the fuelLvl
   *fuelLvl -= (unsigned short)fuelCost;
