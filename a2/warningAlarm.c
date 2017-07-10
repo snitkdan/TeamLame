564497655a98afcc3ad9d056800252777d8b9a2d
Daniel <snitkdan@uw.edu>
2017-07-10 13:56:07 -0700

    Variable declarations for warningAlarm

--- a2/warningAlarm.c
+++ a2/warningAlarm.c
@@ -3,49 +3,19 @@
 #include "dataStructs.h"
 
 
-// Refer to p.15
-void warningAlarm(void *warnStruct)
-{
-  //  turn LED3 on;
-/*
-    PECKOLS ORIGINAL SPEC
-    // all good case
-    if (batt lvl > 50 && fuel lvl > 50) {
-        print and hold LED3
-    } else if (batt lvl <= 50 && fuel lvl > 50) {
-          if (10 < batt lvl <= 50) {
-              blink LED2 every 1 sec
-          } else { //(batt lvl <= 10) {
-              blink LED1 every 1 sec
-              set battlow = true;
-          }
-    } else if (batt lvl > 50 && fuel lvl <= 50) {
-         if (10 < fuel lvl <= 50) {
-              blink LED2 every 2 sec
-         } else { //fuel lvl <= 10
-              blink LED1 every 2 sec
-              set fuellow = true;
-         }
-    } else  { //if (batt lvl <= 50 && fuel lv <= 50) {
-         if (10 < fuel lvl <= 50 && 10 < batt lvl <= 50) {
-              blink LED2 every 1 sec
-         } else if (10 < fuel lvl <= 50 && batt lvl <= 10) {
-              blink LED2 every 2 sec
-              blink LED1 every 1 sec
-              set battlow = true;
+void warningAlarm(void *warnStruct) {
+  // 0. Store warning data in local variables
+  warnData *wData = (warnData *)warnStruct;
+  bool *fuelLowPtr = wData->fuelLowPtr;
+  bool *batteryLowPtr = wData->batteryLowPtr;
+  unsigned short *batteryLvlPtr = wData->batteryLvlPtr;
+  unsigned short *fuelLvlPtr = wData->fuelLvlPtr;
 
-         } else if ( 10 < batt lvl <= 50 && fuel lvl <= 10 )
-              blink LED2 every 1 sec
-              blink LED1 every 2 sec
-              set fuelLow = true;
-         } else { // both are less than 10
-              blink LED1 every 1 sec
-              set battlow = true;
-              set fuelLow = true;
-         }
-    }
-*/
-//   DOUGLAS' EDITED SPEC (PENDING)
+  // 1. Interrogate batteryLvl && fuelLvl
+  switch(*batteryLvl) {
+      case
+  }
+
 /*
      if (batt Lvl > 50 && fuelLvl > 50) {
          turn on LED 3
