2ef601ba182a4b218aee5450692a6833407431a7
Guest <guest-7fz0vp@dhcp141-128.ee.washington.edu>
2017-07-11 18:20:51 -0700

    Phase 1: LEDs

--- a2/warningAlarm.c
+++ a2/warningAlarm.c
@@ -13,10 +13,10 @@
 
 void warningAlarm(void *warnStruct) {
   // 1. Declare structures to store LED metadata
-  static LED led1 = {"sys/class/leds/beaglebone:green:usr1/brightness", NULL, 0, false};
-  static LED led2 = {"sys/class/leds/beaglebone:green:usr2/brightness", NULL, 0, false};
-  static LED led3 = {"sys/class/leds/beaglebone:green:usr3/brightness", NULL, 0, false};
-  static LED *leds[3] = {&led1, &led2, &led3};
+  static LED led1 = {"/sys/class/leds/beaglebone:green:usr1/brightness", NULL, 0, false};
+  static LED led2 = {"/sys/class/leds/beaglebone:green:usr2/brightness", NULL, 0, false};
+  static LED led3 = {"/sys/class/leds/beaglebone:green:usr3/brightness", NULL, 0, false};
+  static LED *leds[3] =+- {&led1, &led2, &led3};
 
   // 3. Update warnStruct && LED state.
   update((warnData*)warnStruct, leds);
@@ -42,7 +42,6 @@ void update(warnData *wData, LED *leds[]) {
 
   // 2: If no alert.
   if(*batteryLvlPtr > 50 && *fuelLvlPtr > 50) {
-    //led3.file = fopen(led3.path);
     leds[2]->active = true;
     leds[2]->sec = -1; // "infinity"
     *fuelLowPtr = false;
@@ -52,29 +51,15 @@ void update(warnData *wData, LED *leds[]) {
   else {
     if(*batteryLvlPtr <= 50) {
       // 3.1: Battery level low
-      //led2.file = fopen(led2.path);
       leds[1]->active = true;
       leds[1]->sec = (*batteryLvlPtr <= 10) ? 1 : 2;
       *batteryLowPtr = (*batteryLvlPtr <= 10) ? true : false;
     }
     if(*fuelLvlPtr <= 50) {
       // 3.2: Fuel level low
-      //led1.file = fopen(led1.path);
       leds[0]->active = true;
       leds[0]->sec = (*fuelLvlPtr <= 10) ? 1 : 2;
       *fuelLowPtr = (*fuelLvlPtr <= 10) ? true : false;
     }
   }
-}
-
-void display(LED *led) {
-
-}
-
-void deactivate(LED *led) {
-  // 0. Close the connection (turn off LED)
-  // fclose(led->file);
-  // 1. Reassign struct fields
-  led->file = NULL;
-  led->active = false;
 }
