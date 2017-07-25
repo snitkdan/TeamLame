137e048120a0246830bcc7a0cd0c06bf3b01c112
abdul <abdul@D-173-250-180-10.dhcp4.washington.edu>
2017-07-25 15:34:09 -0700

    comments added

--- a3/solarPanelControl.c
+++ a3/solarPanelControl.c
@@ -28,20 +28,22 @@ void solarPanelControl(void *solarStruct) {
     // Have fun Abdul - Love, Rad
 
 	
-
+    // Check if the solor panel state with what it is requested to do 
 	if ((*solarPanelState == 1 && *solarPanelDeploy == 1) || (*solarPanelState == 0 && *solarPanelRetract == 1)){
 
 		PWM = 0;
 	}else{
+		//if need speed to increase then duty (run time ) should decrease 
 		if(*motorInc == 1){
 			duty = duty - ((5*duty)/100);
 			duty = duty > 100 ? 100 : duty;
-		}elseif(*motorDec == 1){
+		} else if(*motorDec == 1){
+			//if need speed to decrease then duty (run time ) should increase 
 			duty = duty + ((5*duty)/100);
 			duty = duty < 0 ? 0 : duty;
 		}  
 	}
-
+	//genrate the new PWM for the new duty 
 	PWM = duty * period;
 
 	
