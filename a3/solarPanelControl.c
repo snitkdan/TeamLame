0d18e1b741e1f940b6a6cbd328c27c3b880e0382
Daniel <snitkdan@uw.edu>
2017-07-26 15:59:13 -0700

    First draft PWM

--- a3/solarPanelControl.c
+++ a3/solarPanelControl.c
@@ -6,15 +6,24 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <errno.h>
+#include <string.h>
 #include "dataStructs.h"
 #include "TCB.h"
 #include "solarPanelControl.h"
 
+
+
+
+
+// PWM Functions (inspired by Gavin Strunk: https://class.ee.washington.edu/474/peckol/code/BeagleBone/ForestExamples/Example_4_PWM/Example_4_PWM/libBBB.c)
+static bool initPWM(int mgrnum, char* pin);
+static bool setPWMProperty(int helpnum, char *pin, char *prop, int prop_val);
+
+#define DEVICES "/sys/devices"
 #define MAX 300
-#define pwm_path "/sys/devices/bone_capemgr.9/slots"
-#define duty_path "/sys/devices/ocp.3/pwm_test_P8_13.15/duty"
-#define period_path "/sys/devices/ocp.3/pwm_test_P8_13.15/period"
-#define run_path "/sys/devices/ocp.3/pwm_test_P8_13.15/run"
+#define PIN "P8_13"
+#define HNUM 14
+#define MGRNUM 8
 
 void solarPanelControl(void *solarStruct) {
 	// Only run this function every major cycle
@@ -54,45 +63,49 @@ void solarPanelControl(void *solarStruct) {
 	}
 	// 1.4: Generate the new PWM for the new duty
 	PWM = duty * period;
-	setPWM(duty);
+	initPWM(MGRNUM, PIN);
+	setPWMProperty(HNUM, PIN, "duty", duty);
 }
 
-bool setPWM(double d) {
-
-	#ifndef BEAGLEBONE
-	#define BEAGLEBONE
-	FILE *pwm = fopen(pwm_path, "w");
-	if(!pwm) return false;
-	fseek(pwm,0,SEEK_SET);
-	fprintf(pwm,"am33xx_pwm");
+static bool initPWM(ing mgrnum, char *pin) {
+	// 1. Declare necessary variables
+	FILE *pwm;
+	char mgr_path[50];
+	char pin_path[20];
+	// 2. Construct file paths for the mgr & pin
+	sprintf(mgr_path, "%s/%s/%i/slots", DEVICES, "bone_capemgr.", mgrnum);
+	sprintf(pin_path, "bone_pwm_%s", pin);
+	// 3. Open the pwm
+	pwm = fopen(mgr_path, "w");
+	if(!pwm) {
+		fprintf(stderr, "Failed to initialize PWM for pin %s\n", pin);
+		return false;
+	}
+	// 4. Configure the PWM
+	fprintf(pwm, "am33xx_pwm");
+	fprintf(pwm, "%s", pin_path);
 	fflush(pwm);
-	fprintf(pwm,"bone_pwm_P8_13");
-	fflush(pwm);
-	FILE *period = fopen(period_path, "w");
-	if(!period) return false;
-	fseek(period,0,SEEK_SET);
-	fprintf(period,"%d",500000);
-	fflush(period);
-	FILE *duty = fopen(duty_path, "w");
-	if(!duty) return false;
-	fseek(duty,0,SEEK_SET);
-	fprintf(duty,"%d",250000);
-	fflush(duty);
-	FILE *run = fopen(run_path, "w");
-	fseek(run,0,SEEK_SET);
-	fprintf(run,"%d",0);
-	fflush(run);
-	#endif
+	// 5. Clean up
+	fclose(pwm);
+	return true;
+}
 
-	// 1. Set the duty
-	fseek(duty,0,SEEK_SET);
-	fprintf(duty,"%d",d);
-	fflush(duty);
-	// 2. Run the PMW
-	fseek(run,0,SEEK_SET);
-	fprintf(run,"%d",1);
-	fflush(run);
-	fprintf(run,"%d",0);
-	fflush(run);
+static bool setPWMProperty(int helpnum, char *pin, char *prop, int prop_val) {
+	// 1. Declare necessary variables
+	FILE *pwm;
+	char prop_path[60];
+	// 2. Construct the file path for desired property
+	sprintf(prop_path, "%s/ocp.2/pwm_test_%s.%i/%s", DEVICES, pin, helpnum, prop);
+	// 3, Open the pwm
+	pwm = fopen(prop_path, "w");
+	if(!pwm) {
+		fprintf(stderr, "Failed to update %s\n", prop);
+		return false;
+	}
+	// 4. Set the property
+	fprintf(pwm, "%d", prop_val);
+	fflush(pwm);
+	// 5. Clean up
+	fclose(pwm);
 	return true;
 }
