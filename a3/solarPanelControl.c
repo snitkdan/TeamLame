e97991cd02e10e08be9648081a1603724a41f2c9
Daniel <snitkdan@uw.edu>
2017-07-25 17:11:20 -0700

    First draft PWM

--- a3/solarPanelControl.c
+++ a3/solarPanelControl.c
@@ -5,9 +5,12 @@
 
 #include <stdio.h>
 #include <stdbool.h>
+#include <errno.h>
 #include "dataStructs.h"
 #include "TCB.h"
+
 #define MAX 300
+#define pwm_path "/sys/devices/bone_capemgr.9/slots"
 
 void solarPanelControl(void *solarStruct) {
 	// Only run this function every major cycle
@@ -15,34 +18,48 @@ void solarPanelControl(void *solarStruct) {
 	if((GLOBALCOUNTER - start) % MAJOR_CYCLE != 0) {
       return;
 	}
-    start = GLOBALCOUNTER;
+  start = GLOBALCOUNTER;
 
-    // 1.1 Assign the data of consoleStruct into local variables
-    solarData *solData = (solarData*)solarStruct;
-    bool *solarPanelState = solData->solarPanelStatePtr;
-    bool *solarPanelDeploy = solData->solarPanelDeployPtr;
-    bool *solarPanelRetract = solData->solarPanelRetractPtr;
-    bool *motorInc = solData->motorIncPtr;
-    bool *motorDec = solData->motorDecPtr;
+  // 1.1 Assign the data of consoleStruct into local variables
+  solarData *solData = (solarData*)solarStruct;
+  bool *solarPanelState = solData->solarPanelStatePtr;
+  bool *solarPanelDeploy = solData->solarPanelDeployPtr;
+  bool *solarPanelRetract = solData->solarPanelRetractPtr;
+  bool *motorInc = solData->motorIncPtr;
+  bool *motorDec = solData->motorDecPtr;
 
-    // Have fun Abdul - Love, Rad
+	// 1.2: Declare variables
+	double PWM, duty, period;
 
-
-    // Check if the solor panel state with what it is requested to do
+  // 1.3: Check if the solor panel state with what it is requested to do
 	if ((*solarPanelState == 1 && *solarPanelDeploy == 1) || (*solarPanelState == 0 && *solarPanelRetract == 1)){
-
 		PWM = 0;
-	}else{
+	} else {
 		//if need speed to increase then duty (run time ) should decrease
-		if(*motorInc == 1){
-			duty = duty - ((5*duty)/100);
-			duty = duty > 100 ? 100 : duty;
-		} else if(*motorDec == 1){
+		if(*motorInc == 1) {
+			duty -= ((5 * duty) / 100);
+			duty = (duty > 100) ? 100 : duty;
+		} else if (*motorDec == 1) {
 			//if need speed to decrease then duty (run time ) should increase
-			duty = duty + ((5*duty)/100);
-			duty = duty < 0 ? 0 : duty;
+			duty += ((5*duty) / 100);
+			duty = (duty < 0) ? 0 : duty;
 		}
 	}
-	//genrate the new PWM for the new duty
+	// 1.4: Generate the new PWM for the new duty
 	PWM = duty * period;
+}
+
+bool setPWM(double PWM) {
+	#ifdef BEAGLEBONE
+	FILE *f = fopen(pwm_path, "w");
+	if (!f) {
+		perror("");
+		return false;
+	}
+	fprintf(f, PWM);
+	fflush(f);
+	fclose(f);
+	return true;
+	#endif
+	return false;
 }
