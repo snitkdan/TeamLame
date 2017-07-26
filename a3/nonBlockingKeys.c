606169751bce99f60a66a993df22a8d905e57535
radleigh <radleigh@msn.com>
2017-07-25 19:11:43 -0700

    added user input functionality thats actually working for once in my damn life

--- a3/nonBlockingKeys.c
+++ a3/nonBlockingKeys.c
@@ -12,7 +12,7 @@ int kbhit() {
     fd_set fds;
     tv.tv_sec = 0;
     tv.tv_usec = 0;
-    FD_ZERO(&fds);
+    //FD_ZERO(&fds);
     FD_SET(STDIN_FILENO, &fds); //STDOUT_FILENO is 0
     select(STDIN_FILENO+1, &fds, NULL, NULL, &tv);
     return FD_ISSET(STDIN_FILENO, &fds);
