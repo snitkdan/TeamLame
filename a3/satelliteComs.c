209269cdd6f468a23a1ca56417ec6f6da19feec0
radleigh <radleigh@msn.com>
2017-07-31 01:44:45 -0700

    added satelliteComs warninglow alerts

--- a3/satelliteComs.c
+++ a3/satelliteComs.c
@@ -81,8 +81,11 @@ void satelliteComs(void *satStruct) {
 	"Battery Level: %3u, "
 	"Fuel Level: %3hu, "
 	"Power Consumption: %2hu, "
-	"Power Generation: %2hu\n",
-	 solarPanelString, batteryBuff[(current_measurement > 0 ? current_measurement - 1 : 0)] * 20, *fuelLvl, *pConsume, *pGenerate);
+	"Power Generation: %2hu\n"
+	"Battery Low: %s "
+	"Fuel Low: %s ",
+	 solarPanelString, batteryBuff[(current_measurement > 0 ? current_measurement - 1 : 0)] * 20, *fuelLvl, *pConsume, *pGenerate,
+	 battString, fuelString);
 
 	 if (*response == 'A') {
 		 dprintf(fd1, "\nVehicle Response: %c %c\n", *response, *command);
