f09a69f5954e4e485cbc48ffae1fc292f51dff6a
radleigh <radleigh@msn.com>
2017-08-03 17:36:02 -0700

    moved Daniels Makefile and replaced radMakefile

--- a4/batteryTemp.c
+++ a4/batteryTemp.c
@@ -23,7 +23,22 @@ void batteryTemp(void *temperatureStruct) {
     start = GLOBALCOUNTER;
 	*/
     tempData *tData = (tempData*)temperatureStruct;
-    bool *batteryOverTemp = tData->batteryOverTempPtr;    	
+    bool *batteryOverTemp = tData->batteryOverTempPtr;
+    
+    #ifdef THIS_IS_PSUEDOCODE
+	int sampleBuffer1[16];
+	int sampleBuffer2[16];
+	// Make buffers circular with fifo?
+	battTemp1 = measure from sensor1;
+	battTemp2 = measure from sensor2;
+	temp1 = 32 * battTemp1 + 33;
+	temp2 = 32 *battTemp2 + 33;
+	if (temp1 > 20 * previousTemp1 /100 || temp2 > 20 * previousTemp2/100) {
+				*batteryOverTemp = true;
+	}  else {
+				*batteryOverTemp = false;
+	}
+    #endif	
     return;
 }
 
